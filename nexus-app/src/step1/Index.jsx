28556a9502dbc1a29dbfcffcd1120e6c7fa2bfff
Ishwari <ishwarinandargi@gmail.com>
2026-02-22 12:04:43 +0530

    Added inputs to Calculator

--- nexus-app/src/step1/Index.jsx
+++ nexus-app/src/step1/Index.jsx
@@ -6,6 +6,25 @@ import Division from "./Division";
 let Calculator = () => {
   return (
     <>
+      <div className="flex justify-center">
+        <div className="border-3 border-blue-900 rounded-xl shadow-2xl m-4 w-1/4">
+          <h1 className="bg-blue-900 text-center text-xl text-white rounded-t-md">
+            Calculator App
+          </h1>
+          <div className="flex">
+            <input
+              type="text"
+              className="border-1 border-blue-900 p-3 m-2 text-center w-1/2 rounded-md"
+              placeholder="num1"
+            />
+            <input
+              type="text"
+              className="border-1 border-blue-900 p-3 m-2 text-center w-1/2 rounded-md"
+              placeholder="num2"
+            />
+          </div>
+        </div>
+      </div>
       <h1>Calculator </h1>
       <Addition />
       <Subtraction />
